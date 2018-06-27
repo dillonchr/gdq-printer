a1ccc0e77cc82518c80051ae0d742180ba5c9ab7
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-06-27 16:56:02 -0400

    prints now, needs 7 more dashes, and cleanup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const gdq = require('@dillonchr/gdq');
 const moment = require('moment');
+const {exec} = require('child_process');
 
 module.exports = () => {
     gdq((err, runs) => {
@@ -7,7 +8,7 @@ module.exports = () => {
             console.error(err);
         } else {
             const today = new Date().getDate();
-            const runsToday = [`SGDQ 2018 - ${moment().format('MMM D')}`];
+            const runsToday = [`SGDQ 2018 - ${moment().utcOffset('-05:00').format('MMM D')}`];
             const len = runs.length;
             let i = 0;
 
@@ -17,14 +18,20 @@ module.exports = () => {
                 if (d.getDate() > today) {
                     break;
                 } else if (!run.done) {
-                    runsToday.push(`( ) ${run.title}\n-------------------------\n${moment(run.start).format('h:mm A')} - ${moment(run.ends).format('h:mm A')}`);
+                    runsToday.push(`( ) ${run.title}\n-------------------------\n${moment(run.start).utcOffset('-05:00').format('h:mm A')} - ${moment(run.ends).utcOffset('-05:00').format('h:mm A')}`);
                 }
                 i++;
             }
 
             console.log(runsToday.join('\n\n'));
+
+            exec(`echo "${runsToday.join('\n\n')}\n\n" > /dev/usb/lp0`, (err, stdout, stderr) => {
+                if (err) console.log(500, 'print error ' + err);
+                if (stderr) console.log(500, 'print error std ' + stderr);
+                if (!stderr && !err) console.log(200, 'think we printed');
+            });
         }
-    });  
+    });
 };
 
 module.exports();
