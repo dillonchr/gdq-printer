83f39c9ca08867a71b1b162d372a04b13f71a1ca
Dillon Christensen <j.dillon.christensen@gmail.com>
2019-01-12 15:50:50 -0600

    finally got around to making lines wrap more prettily

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 const gdq = require('@dillonchr/gdq');
 const moment = require('moment');
 const {exec} = require('child_process');
+const maxWidth = 32;
+
+const receiptFormatter = text => {
+    const result = text
+        .split('\n')
+        .map(line => {
+            if (line.length > maxWidth) {
+                return line.split(' ').reduce((whole, word, i) => {
+                    if (!i) {
+                        return word;
+                    }
+                    const lastNewLine = whole.lastIndexOf('\n') !== -1 ? whole.lastIndexOf('\n') : 0;
+                    const lengthSoFar = whole.substr(lastNewLine + 1).length;
+                    const wouldBeNextLineLength = lengthSoFar + word.length + 1;
+                    if (wouldBeNextLineLength > maxWidth) {
+                        return `${whole}\n    ${word}`;
+                    }
+                    return `${whole} ${word}`;
+                });
+            }
+
+            return line;
+        })
+        .join('\n');
+    return result;
+};
 
 module.exports = () => {
     gdq((err, runs) => {
@@ -15,7 +41,7 @@ module.exports = () => {
             const tomorrow = new Date(now + (1000 * 60 * 60 * 24));
             const AorS = tomorrow.getMonth() ? 'S' : 'A';
             const year = tomorrow.getFullYear();
-            const runsToday = runs
+            const runsToday = receiptFormatter(runs
                 .filter(r => {
                     const ends = new Date(r.ends);
                     const starts = new Date(r.start);
@@ -23,7 +49,7 @@ module.exports = () => {
                 })
                 .reduce((list, run) => {
                     return `${list}\n( ) ${run.title}\n    ${moment(run.start).format('h:mm A')} - ${moment(run.ends).format('h:mm A')}\n\n--------------------------------`;
-                }, `${AorS}GDQ ${year} - ${moment().format('MMM D')}`);
+                }, `${AorS}GDQ ${year} - ${moment().format('MMM D')}`));
 
             console.log(runsToday);
 
