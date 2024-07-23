0fd62e763e281255c87655669c2030dd09d6e827
Dillon Christensen <j.dillon.christensen@gmail.com>
2024-07-23 09:01:41 -0500

    esa updates

--- esa-main.js
+++ esa-main.js
@@ -1,14 +1,14 @@
-const gdq = require('@dillonchr/gdq')
+const gdq = require("@dillonchr/gdq");
 const esa = require("./esa.js");
 const moment = require("moment");
 const maxWidth = 32;
 const RUN_FORMAT = "H:mm";
 const BONUS_GAME = /Bonus Game \d/;
 
-const receiptFormatter = text => {
+const receiptFormatter = (text) => {
   const result = text
     .split("\n")
-    .map(line => {
+    .map((line) => {
       if (line.length > maxWidth) {
         return line.split(" ").reduce((whole, word, i) => {
           if (!i) {
@@ -30,71 +30,53 @@ const receiptFormatter = text => {
   return result;
 };
 
-const spaces = n => Array(n).fill(" ").join("");
+const spaces = (n) => Array(n).fill(" ").join("");
 
-const spaceBetween = (w1, w2) =>
-  w1 + spaces(maxWidth - (w1.length + w2.length)) + w2;
+const spaceBetween = (w1, w2) => w1 + spaces(maxWidth - (w1.length + w2.length)) + w2;
 
 function eventName(acronym) {
   const isWinter = moment().format("M") < 6;
   return (
     spaceBetween(
-      "esa" === acronym
-        ? `ESA ${isWinter ? "WINTER" : "SUMMER"}`
-        : `${isWinter ? "A" : "S"}GDQ`,
+      "esa" === acronym ? `ESA ${isWinter ? "WINTER" : "SUMMER"}` : `${isWinter ? "A" : "S"}GDQ`,
       moment().format("ddd MMM D")
     ) + "\n"
   );
 }
 
-module.exports = eventCode => {
+module.exports = (eventCode) => {
   const fetch = "esa" === eventCode ? esa : gdq;
   fetch((err, runs) => {
     if (err) {
       console.error(err);
     } else {
-      if (!runs.length || !runs.filter(g => !g.done).length) {
+      if (!runs.length || !runs.filter((g) => !g.done).length) {
         process.exit(0);
       }
 
       const now = moment();
       const endOfSchedule = moment().add(24, "hours");
-      const includedInToday = starts => endOfSchedule.isAfter(starts);
+      const includedInToday = (starts) => endOfSchedule.isAfter(starts);
       const runsToday = receiptFormatter(
         runs
-          .filter(r => {
+          .filter((r) => {
             return !r.done && includedInToday(r.start);
           })
           .reduce((list, run) => {
-            const estimate = run.estimate
-              .split(":")
-              .reduce((str, metric, i) => {
-                switch (i) {
-                  case 0:
-                    return metric > 0 ? `${metric}h` : str;
-                  case 1:
-                    return `${str}${metric}m`;
-                  default:
-                    return str;
-                }
-              }, "");
-            const runTimes = `    ${run.start.format(
-              RUN_FORMAT
-            )} - ${run.ends.format(RUN_FORMAT)}`;
-            const spacePadding = Math.max(
-              maxWidth - (estimate.length + runTimes.length),
-              0
-            );
-            const runTitle = BONUS_GAME.test(run.title)
-              ? "??? Bonus\n    ???"
-              : run.title;
-            return `${list}\n(${
-              "esa" === eventCode
-                ? "2023-winter1" === run.stream
-                  ? 1
-                  : 2
-                : " "
-            }) ${runTitle}\n${runTimes}${spaces(
+            const estimate = run.estimate.split(":").reduce((str, metric, i) => {
+              switch (i) {
+                case 0:
+                  return metric > 0 ? `${metric}h` : str;
+                case 1:
+                  return `${str}${metric}m`;
+                default:
+                  return str;
+              }
+            }, "");
+            const runTimes = `    ${run.start.format(RUN_FORMAT)} - ${run.ends.format(RUN_FORMAT)}`;
+            const spacePadding = Math.max(maxWidth - (estimate.length + runTimes.length), 0);
+            const runTitle = BONUS_GAME.test(run.title) ? "??? Bonus\n    ???" : run.title;
+            return `${list}\n( ) ${runTitle}\n    ${run.runners}\n${runTimes}${spaces(
               spacePadding
             )}${estimate}\n\n--------------------------------`;
           }, eventName(eventCode))
--- esa.js
+++ esa.js
@@ -1,9 +1,6 @@
 const moment = require("moment");
 const fetch = require("@dillonchr/fetch");
-const urls = [
-  "https://app.esamarathon.dev/horaro-proxy/v2/esa/schedule/2023-summer1",
-  "https://app.esamarathon.dev/horaro-proxy/v2/esa/schedule/2023-summer2"
-];
+const urls = ["https://app.esamarathon.dev/horaro-proxy/v2/esa/schedule/2024-summer"];
 
 /*
  * {
@@ -17,32 +14,28 @@ const urls = [
   */
 
 function deMarkdown(str) {
-  return null != str ?
-    str.replace(/^\[/, "").replace(/\].*$/, "") :
-    "";
+  return null != str ? str.replace(/^\[/, "").replace(/\].*$/, "") : "";
 }
 
 function prettyLength(length) {
-  return [
-    String(Math.floor(length / 3600)),
-    String((length / 60) % 60),
-    String(length % 60).padStart(2, "0")
-  ].join(":");
+  return [String(Math.floor(length / 3600)), String((length / 60) % 60), String(length % 60).padStart(2, "0")].join(
+    ":"
+  );
 }
 
 function streamToGames(stream) {
   const now = moment();
   const scheduleJson = "string" === typeof stream ? JSON.parse(stream) : stream;
-  return scheduleJson.data.map(game => {
+  return scheduleJson.data.map((game) => {
     const ends = moment(game.scheduled).add(game.length, "s");
     return {
       stream: scheduleJson.meta.slug,
       start: moment(game.scheduled),
       title: deMarkdown(game.game),
-      runners: deMarkdown(game.players[0]),
+      runners: game.players.map(deMarkdown).join(", "),
       estimate: prettyLength(game.length),
       ends,
-      done: now.isAfter(ends)
+      done: now.isAfter(ends),
     };
   });
 }
@@ -52,20 +45,8 @@ function getEsaSchedules(callback) {
     if (err) {
       callback(err);
     } else {
-      fetch({ url: urls[1] }, (err, stream2) => {
-        if (err) {
-          callback(err);
-        } else {
-          // we have stream1 and stream2 here
-          callback(
-            null,
-            [stream1, stream2]
-              .map(streamToGames)
-              .reduce((schedule, stream) => schedule.concat(stream), [])
-              .sort((a, b) => a.start - b.start)
-          );
-        }
-      });
+      // we have stream1 and stream2 here
+      callback(null, streamToGames(stream1));
     }
   });
 }
