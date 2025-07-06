f4fc23657cf06f77993ed5fc732b2fc5b656291c
Dillon Christensen <dillon@wiredquote.com>
2025-07-06 15:31:06 -0400

    checking in old changes

--- esa-today.sh
+++ esa-today.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R $(whoami) /dev/usb/lp0
-/home/pi/git/png2pos/print.py sumz.png > /dev/usb/lp0
+# chown -R $(whoami) /dev/usb/lp0
+# /home/beluga/git/png2pos/print.py sumz.png > /dev/usb/lp0
 node esa-main.js
 
